e7aacf8f482c40bcad848727a4024d920f745bea
BinguniAthapaththu <binguniathapaththu@gmail.com>
2024-09-12 13:46:43 +0530

    Adding Login Verification, AddToCartButton Verification, RemoveFromCart Verification, and Logout Verification

--- src/test/java/SeleniumDemo.java
+++ src/test/java/SeleniumDemo.java
@@ -21,6 +21,16 @@ public class SeleniumDemo {
         // Test Case 1: Login Verification
         validLogin(driver, "standard_user", "secret_sauce");
 
+        // Test Case 2: AddToCartButton Verification
+        addToCart(driver);
+
+        // Test Case 3: RemoveFromCart Verification
+        removeFromCart(driver);
+
+        // Test Case 4: Logout Verification
+        logout(driver);
+
+        driver.close();
     }
 
     public static void validLogin(WebDriver driver, String username, String password) {
@@ -40,5 +50,49 @@ public class SeleniumDemo {
         }
     }
 
+    public static void addToCart(WebDriver driver){
+        WebElement addToCartButton = driver.findElement(By.id("add-to-cart-sauce-labs-backpack"));
+        addToCartButton.click();
 
+        WebElement cartBadge = driver.findElement(By.className("shopping_cart_badge"));
+
+        if(cartBadge.getText().equals("1")){
+            System.out.println("Test Case 2 Pass");
+        }
+        else {
+            System.out.println("Test Case 2 Fail");
+        }
+    }
+
+    public static void removeFromCart(WebDriver driver){
+        WebElement removeButton = driver.findElement(By.id("remove-sauce-labs-backpack"));
+        removeButton.click();
+
+        boolean isCartBadgePresent = driver.findElements(By.className("shopping_cart_badge")).isEmpty();
+
+        if(isCartBadgePresent){
+            System.out.println("Test Case 3 Pass");
+        }
+        else {
+            System.out.println("Test Case 3 Fail");
+        }
+    }
+
+    public static void logout(WebDriver driver){
+        WebElement menuButton = driver.findElement(By.id("react-burger-menu-btn"));
+        menuButton.click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("logout_sidebar_link")));
+
+        WebElement logOutButton = driver.findElement(By.id("logout_sidebar_link"));
+        logOutButton.click();
+
+        if(driver.getCurrentUrl().equals("https://www.saucedemo.com/")){
+            System.out.println("Test Case 4 Pass");
+        }
+        else {
+            System.out.println("Test Case 4 Fail");
+        }
+    }
 }
